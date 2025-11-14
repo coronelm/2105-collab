a803f307137e2430ce4099f842a4632d109ff60c
Duwidoughnut <roxasdwaynewesley@gmail.com>
2025-11-14 17:32:09 +0800

    Fixed the typo in inserted student records. typo: extra parenthesis

--- schema.sql
+++ schema.sql
@@ -5,5 +5,5 @@ CREATE TABLE students (
  grade_level VARCHAR(10)
 );
 INSERT INTO students (student_id, student_name, grade_level) VALUES (31859, 'Dwayne', '2nd Year');
-INSERT INTO students ((student_id, student_name, grade_level) VALUES (39377, 'Timothy', '2nd Year');
+INSERT INTO students (student_id, student_name, grade_level) VALUES (39377, 'Timothy', '2nd Year');
 INSERT INTO students (student_id, student_name, grade_level) VALUES(31026, 'Gerald', '2nd Year');
