489e5932f917c8a47108a31493fee3e6cf34b35f
raemarie-g <gallosaraemarie@gmail.com>
2025-11-14 15:51:52 +0800

    Group 4 created enrollment table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE school_db;USE school_db;
+CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
+
