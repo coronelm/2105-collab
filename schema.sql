568627da6781c08577b870c548e5cada8528734e
sofiajanereobaldes <24-36961@g.batstate-u.edu.ph>
2025-11-14 20:06:58 +0800

    Group4-Gallosa: Created enrollments table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db;USE school_db;
 CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
 
+CREATE TABLE enrollments(enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
