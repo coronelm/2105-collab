fac350fac7ca91568ef196db2210916263d24ccd
zyy-cueto <aizycueto5@gmail.com>
2025-11-14 17:26:56 +0800

    Aizy updated status for attendance id 2

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;
 CREATE TABLE attendance (attendance_id INT PRIMARY KEY, student_id INT, date DATE, status VARCHAR(50));
 INSERT INTO attendance (attendance_id, student_id, date, status) VALUES (1, 101, '2025-11-10', 'Present'), (2, 102, '2025-11-11', 'Absent'), (3, 103, '2025-11-12', 'Late');
 ALTER TABALE attendance Add remarks VARCHAR(50);
+UPDATE attendance SET status = 'Late' WHERE attendance_id = 2;
