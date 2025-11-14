20c0667b517683d722b0a0900008e097869d702c
diwataparesoverload1 <24-31399@g.batstate-u.edu.ph>
2025-11-14 20:19:56 +0800

    Vince added teachers records.

--- teachers.sql
+++ teachers.sql
@@ -3,3 +3,6 @@ CREATE TABLE teachers (
     name VARCHAR(100),
     subject VARCHAR(100)
 );
+INSERT INTO teachers VALUES (1, 'Mr. Jexus', 'ACP');
+INSERT INTO teachers VALUES (2, 'Ms. Marielle', 'DBMS');
+INSERT INTO teachers VALUES (3, 'Mr. Baluyot', 'Physics');
