9ab5e627d43aa3efa77c6a465f34a38a9bf4f5db
maynabellemalacas <maynabellemalacas@gmail.com>
2025-11-14 21:10:28 +0800

    Group 6-Malacas Inserted departments table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;USE school_db;
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR(50));
+INSERT INTO departments (department_id, department_name) VALUES (101, 'CAS'), (102, 'CABE'), (103, 'CICS);
