07a15e55ad5258bb252b726e0a3609438d598ba8
Duwidoughnut <roxasdwaynewesley@gmail.com>
2025-11-14 17:25:51 +0800

    Member 2 successfully inserted 2-3 sample records

--- schema.sql
+++ schema.sql
@@ -4,3 +4,6 @@ CREATE TABLE students (
  student_name VARCHAR(100),
  grade_level VARCHAR(10)
 );
+INSERT INTO students (student_id, student_name, grade_level) VALUES (31859, 'Dwayne', '2nd Year');
+INSERT INTO students ((student_id, student_name, grade_level) VALUES (39377, 'Timothy', '2nd Year');
+INSERT INTO students (student_id, student_name, grade_level) VALUES(31026, 'Gerald', '2nd Year');
