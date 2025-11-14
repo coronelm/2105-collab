5355758b4b5f6371e41ce6e0a064a6391d2cbe02
sxxamm01 <espisam01@gmail.com>
2025-11-14 17:06:35 +0800

    Fix typo: corrected 'reamrks' to 'remarks'

--- grades.sql
+++ grades.sql
@@ -1,4 +1,4 @@
 CREATE TABLE grades (grade_id INT PRIMARY KEY, student_id INT, subject_id INT, score INT);
 INSERT INTO grades VALUES (1, 101, 501, 90), (2, 102, 501, 80), (3, 103, 502, 92);
 ALTER TABLE grades ADD remarks VARCHAR(50);
-UPDATE grades SET reamrks = 'PASSED' WHERE grade_id =1;
+UPDATE grades SET remarks = 'PASSED' WHERE grade_id = 1;
