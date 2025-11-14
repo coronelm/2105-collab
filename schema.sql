74e4a851fa5e609f4766dc7d23de1a5fbc3a841e
MallariCJ <24-35059@g.batstate-u.edu.ph>
2025-11-14 20:52:32 +0800

    Update Classroom Number by Cassandra Manalo

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN 
  CREATE TABLE classes ( id INT PRIMARY KEY, class_name VARCHAR(100), room_no VARCHAR(10))
 INSERT INTO classes (id, class_name, room_no) VALUES (1, 'Math 101', 'RM-201');
 ALTER TABLE classes ADD COLUMN teacher_id INT;
+UPDATE classes SET room_no = 'RM-210' WHERE id = 1;
