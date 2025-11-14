983c4ab246e37a9136d0c8a16c1daeeed646444d
sofiajanereobaldes <24-36961@g.batstate-u.edu.ph>
2025-11-14 20:08:59 +0800

    Group4-Reobaldes: Inserted enrollments table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;
 CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
 
 CREATE TABLE enrollments(enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
+INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 01, 101), (2, 02, 102), (3, 03, 103);
