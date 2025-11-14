2d2fd1eff9cc4e254cc4d35eeaa0d588b2984d14
zyy-cueto <aizycueto5@gmail.com>
2025-11-14 17:15:27 +0800

    Jhemzy inserted 3 attendance record

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;USE school_db;
 CREATE TABLE attendance (attendance_id INT PRIMARY KEY, student_id INT, date DATE, status VARCHAR(50));
+INSERT INTO attendance (attendance_id, student_id, date, status) VALUES (1, 101, '2025-11-10', 'Present'), (2, 102, '2025-11-11', 'Absent'), (3, 103, '2025-11-12', 'Late');
