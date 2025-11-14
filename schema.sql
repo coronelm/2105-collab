53823a3b68aa3cee9bab72d4a839b1b3b5294b88
sofiajanereobaldes <24-36961@g.batstate-u.edu.ph>
2025-11-14 20:11:40 +0800

    Group4-DeSilva: Updated school_year in enrollments table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN 
 
 CREATE TABLE enrollments(enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 01, 101), (2, 02, 102), (3, 03, 103);
+ALTER TABLE enrollments ADD school_year
