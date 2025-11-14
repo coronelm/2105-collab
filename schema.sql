3ab3c85dc74a78397d0c3a188422f66049e5a07b
hrtgy <escarosheart@gmail.com>
2025-11-14 21:22:35 +0800

    Group 6-Escaros Created Departments Table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db;USE school_db;
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR(50));
 INSERT INTO departments (department_id, department_name) VALUES (101, 'CAS'), (102, 'CABE'), (103, 'CICS);
+CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHARR(50));
