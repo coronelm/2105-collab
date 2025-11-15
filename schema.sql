2e924db2b686b549b4695d81e121878c55e75ebb
jermaineparallon27 <jermaineparallon27@gmail.com>
2025-11-15 21:53:33 +0800

    Member 3 (Abregunda): Added unit column

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN 
 
 CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name TEXT NOT NULL);
 INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Database Management System'), (2, 'Advanced Computer Programming'), (3, 'Object-Oriented Programming');
+ALTER TABLE subjects ADD COLUMN units INT;
