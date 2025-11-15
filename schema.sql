5afa2ce1cb45909e6a80d6e40eb1e8343a09bd29
hrtgy <escarosheart@gmail.com>
2025-11-15 23:00:46 +0800

    Group 6-Nuay Updated Head Teacher Name

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR(50));
 INSERT INTO departments (department_id, department_name) VALUES (101, 'CAS'), (102, 'CABE'), (103, 'CICS);
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHARR(50));
 ALTER TABLE teachers ADD COLUMN head_teacher VARCHAR(100);
+UPDATE departments SET head_teacher = 'Ms. Coronel' WHERE dept_id = '111';
