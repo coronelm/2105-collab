c405b8f28b107496bba1987822f1b2da8af7f630
jermaineparallon27 <jermaineparallon27@gmail.com>
2025-11-15 21:35:52 +0800

    Member 2 (Dimabayao): Inserted 3 subjects

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;
 CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), class_id INT, FOREIGN KEY (class_id) REFERENCES classes(id));
 
 CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name TEXT NOT NULL);
+INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Database Management System'), (2, 'Advanced Computer Programming'), (3, 'Object-Oriented Programming');
