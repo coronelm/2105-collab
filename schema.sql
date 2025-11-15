990e2c6206c23c2b3c6617d66f02480a16279102
maynabellemalacas <maynabellemalacas@gmail.com>
2025-11-15 22:20:23 +0800

    Group 6-Repe Added Head Teachers Table.

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR(50));
 INSERT INTO departments (department_id, department_name) VALUES (101, 'CAS'), (102, 'CABE'), (103, 'CICS);
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHARR(50));
+ALTER TABLE teachers ADD COLUMN head_teacher VARCHAR(100);
