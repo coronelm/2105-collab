befc5f039c96717325c6f6c9c1e075ed0b102ecc
jermaineparallon27 <jermaineparallon27@gmail.com>
2025-11-15 21:55:08 +0800

    Member 4 (Dumas): Updated one subject's units

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, FOREIGN 
 CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name TEXT NOT NULL);
 INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Database Management System'), (2, 'Advanced Computer Programming'), (3, 'Object-Oriented Programming');
 ALTER TABLE subjects ADD COLUMN units INT;
+UPDATE subjects SET units = 3 WHERE subject_id = 1;
